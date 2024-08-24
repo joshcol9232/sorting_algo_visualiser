5784f6d6ff89104f45fc8db85cacba482eef9ef4
mo-joshuacolclough <joshua.colclough@metoffice.gov.uk>
2024-08-24 18:41:10 +0100

    Build as library

--- src/constants.h
+++ src/constants.h
@@ -7,8 +7,8 @@
 
 namespace constants {
 
-  const size_t WINDOW_HEIGHT = 1200;
-  const size_t WINDOW_WIDTH = 1600;
+  const size_t WINDOW_HEIGHT = 600;
+  const size_t WINDOW_WIDTH  = 800;
 
   const size_t NUM_ELEMENTS = 200;
 
